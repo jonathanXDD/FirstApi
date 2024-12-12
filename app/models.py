40b9c468c8930d1a2e38c340737087554acf6439
jonathanXDD <jonathan27.tw@gmail.com>
2024-12-12 14:42:38 +0800

    1212split

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column,Date,Integer,String,Boolean
+from .database import Base
+#define modle
+class Todo(Base):
+    __tablename__="todos"
+    id=Column(Integer,primary_key=True,index=True)
+    title = Column(String(100),nullable=False)
+    description=Column(String,nullable=True)
+    completed=Column(Boolean,default=False)
+    due_date=Column(Date,nullable=True)

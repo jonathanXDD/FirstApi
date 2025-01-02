c74e0c7ce90275be3b9a52ae324f2df8ad5623cc
jonathan Chen <jonathan27.tw@gmail.com>
2025-01-02 14:21:45 +0800

    2025/1/2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
-from app.database import Base,engine
-from app.routers import router
+
+from .database import Base,engine
+from .routers import router
+from .auth import router as auth_router
+
 #initialize 
 
 app = FastAPI()
@@ -10,4 +13,4 @@ Base.metadata.create_all(bind=engine)
 
 #register Router
 app.include_router(router=router,prefix="/api",tags=["todos"])
-
+app.include_router(auth_router)
